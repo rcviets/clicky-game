d6814bb035931b8ee4c07254b8ce5b9bb2822313
Randamus <rcviets@gmail.com>
2020-01-11 10:38:47 -0700

    score component

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Score from 'score';
 
 const Header = () => {
     return (
         <div>
             <nav className="navbar navbar-light bg-danger">
             <a className="navbar-brand" href="#">Clicky Game</a>
+            <span class="navbar-text">
+                <Score />
+            </span>
             </nav>
         </div>
     );
