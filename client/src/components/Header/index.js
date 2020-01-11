382e033eef2450f7d1d1ba378e0e50420aa1d225
Randamus <rcviets@gmail.com>
2020-01-10 20:42:51 -0700

    navbar

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-light bg-danger">
+            <a className="navbar-brand" href="#">Clicky Game</a>
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
