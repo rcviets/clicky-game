8519181f027781c09b623cff6cf46c190d9c180c
Randamus <rcviets@gmail.com>
2020-01-13 17:24:38 -0700

    set state

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Header from './components/Header';
+import Jumbotron from './components/Jumbotron';
+import Score from './components/Score';
+import Officers from './tng.json';
+import './App.css';
+
+class App extends React.Component {
+  state = {
+    topScore: 0,
+    score: 0,
+    body: Officers
+  }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
