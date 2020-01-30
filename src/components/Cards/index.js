2edf98d32ba3e3be818b64847965779697962d73
Randamus <rcviets@gmail.com>
2020-01-29 20:29:26 -0700

    added padding to image cards

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 const Cards = (props) => {
     return (
         <div className="card">
-            <div className="image-container">
+            <div className="image-container pt-4">
                 <img alt={props.name} src={props.image} onClick={() => props.handleClick(props.id)} />
             </div>
         </div>
