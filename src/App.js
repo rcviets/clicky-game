b1823553d2e4368924e0e485136a1e8028eb396a
Randamus <rcviets@gmail.com>
2020-02-02 17:25:16 -0700

    shuffle cards function

--- src/App.js
+++ src/App.js
@@ -43,37 +43,49 @@ class App extends React.Component {
           if (this.state.score <= this.state.topScore) {
             this.setState({
               score: 0,
+              headerText: 'The Line Must Be Drawn Here!',
               officers
-            })
+            });
           }
         }
       }
     }
+  };
+
+  //Shuffle Cards Function
+  shuffleArray = array => {
+    let i = array.length - 1;
+
+    for (; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
   }
   
   
   render() {
     const {score, topScore, headerText} = this.state;
-    let officerCards = this.state.officers.map((officer) =>{
-      console.log(officer.image);
-      return (
-        <Cards 
-          name = {officer.name}
-          image = {officer.image}
-          key = {officer.id}
-          id = {officer.id}
-          handleClick = {this.handleClick}
-        />  
-      )
-    }
+    let officerCards = this.state.officers.map((officer) =>
+      //console.log(officer.image);
+      <Cards 
+        name = {officer.name}
+        image = {officer.image}
+        key = {officer.id}
+        id = {officer.id}
+        handleClick = {this.handleClick}
+      />  
     )
+    let shuffledCards = this.shuffleArray(officerCards)
     return (
       <div>
          {/* score = {score} topScore = {topScore} */}
         <Header {...{score, topScore}} />
         <Jumbotron {...{headerText}} />
         <Wrapper>
-          {officerCards}
+          {shuffledCards}
         </Wrapper>
       </div>
     );
